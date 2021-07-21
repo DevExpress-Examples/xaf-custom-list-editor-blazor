ea8219ac591e882ff8321c88da542751704f09ba
Eugenia.Simonova <eugenia.simonova@devexpress.com>
2021-07-21 16:45:17 +0300

    update

--- CS/MySolution.Module/DatabaseUpdate/Updater.cs
+++ CS/MySolution.Module/DatabaseUpdate/Updater.cs
@@ -7,6 +7,8 @@ using DevExpress.ExpressApp.Updating;
 using DevExpress.Xpo;
 using DevExpress.ExpressApp.Xpo;
 using DevExpress.Persistent.BaseImpl;
+using MySolution.Module.BusinessObjects;
+using System.IO;
 
 namespace MySolution.Module.DatabaseUpdate {
     // For more typical usage scenarios, be sure to check out https://docs.devexpress.com/eXpressAppFramework/DevExpress.ExpressApp.Updating.ModuleUpdater
@@ -16,14 +18,7 @@ namespace MySolution.Module.DatabaseUpdate {
         }
         public override void UpdateDatabaseAfterUpdateSchema() {
             base.UpdateDatabaseAfterUpdateSchema();
-            //string name = "MyName";
-            //DomainObject1 theObject = ObjectSpace.FirstOrDefault<DomainObject1>(u => u.Name == name);
-            //if(theObject == null) {
-            //    theObject = ObjectSpace.CreateObject<DomainObject1>();
-            //    theObject.Name = name;
-            //}
-
-			//ObjectSpace.CommitChanges(); //Uncomment this line to persist created object(s).
+            CreateCustomListEditorObjects();
         }
         public override void UpdateDatabaseBeforeUpdateSchema() {
             base.UpdateDatabaseBeforeUpdateSchema();
@@ -31,5 +26,47 @@ namespace MySolution.Module.DatabaseUpdate {
             //    RenameColumn("DomainObject1Table", "OldColumnName", "NewColumnName");
             //}
         }
+
+        private void CreateCustomListEditorObjects() {
+            PictureItem image1 = ObjectSpace.FindObject<PictureItem>(CriteriaOperator.Parse("Text='Eternally Yours by Tay Garnett'"));
+            if (image1 == null) {
+                image1 = ObjectSpace.CreateObject<PictureItem>();
+                image1.Text = "Eternally Yours by Tay Garnett";
+                image1.Image = GetImageFromResource("MySolution.Module.ListEditorImages.EternallyYours.png");
+                image1.Save();
+            }
+            PictureItem image2 = ObjectSpace.FindObject<PictureItem>(CriteriaOperator.Parse("Text='Sherlock Holmes - Dressed to Kill by Roy William Neill'"));
+            if (image2 == null) {
+                image2 = ObjectSpace.CreateObject<PictureItem>();
+                image2.Text = "Sherlock Holmes - Dressed to Kill by Roy William Neill";
+                image2.Image = GetImageFromResource("MySolution.Module.ListEditorImages.DressedToKill.png");
+                image2.Save();
+            }
+            PictureItem image3 = ObjectSpace.FindObject<PictureItem>(CriteriaOperator.Parse("Text='The Last Time I Saw Paris by Richard Brooks'"));
+            if (image3 == null) {
+                image3 = ObjectSpace.CreateObject<PictureItem>();
+                image3.Text = "The Last Time I Saw Paris by Richard Brooks";
+                image3.Image = GetImageFromResource("MySolution.Module.ListEditorImages.TheLastTimeISawParis.png");
+                image3.Save();
+            }
+            PictureItem image4 = ObjectSpace.FindObject<PictureItem>(CriteriaOperator.Parse("Text='Teenagers from Outer Space by Tom Graeff'"));
+            if (image4 == null) {
+                image4 = ObjectSpace.CreateObject<PictureItem>();
+                image4.Text = "Teenagers from Outer Space by Tom Graeff";
+                image4.Save();
+            }
+
+        }
+
+        private byte[] GetImageFromResource(string name) {
+            UnmanagedMemoryStream stream = (UnmanagedMemoryStream)GetType().Assembly.GetManifestResourceStream(name);
+            stream.Position = 0;
+            byte[] result = new byte[stream.Length];
+            stream.Read(result, 0, (Int32)stream.Length);
+            return result;
+        }
+
     }
+
+
 }
